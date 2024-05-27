8b4d7548d346c729c13c2716bf68ac07a5e5edc7
Augusto Latgé <augusto.latge@erudit.org>
2024-05-27 08:42:54 -0400

    Allow multiple images upload

--- images_storage/images/forms.py
+++ images_storage/images/forms.py
@@ -2,10 +2,26 @@ from django import forms
 from .models import Image
 
 
-class UploadForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ['src']
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+class UploadForm(forms.Form):
+    images = MultipleFileField()
 
     def __init__(self, *args, **kwargs):
         if "user" in kwargs:
@@ -13,4 +29,5 @@ class UploadForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
     def save(self, commit=True):
-        Image.objects.create(user=self.user, src=self.cleaned_data['src'])
+        for src in self.cleaned_data["images"]:
+            Image.objects.create(user=self.user, src=src)
--- images_storage/images/urls.py
+++ images_storage/images/urls.py
@@ -6,5 +6,5 @@ app_name = 'images'
 urlpatterns = [
     path('list/', views.images_list, name="list"),
     path('list_by_category/<int:category>', views.images_list_by_category, name="list_by_category"),
-    path('upload/', views.upload_image, name="upload"),
+    path('upload/', views.upload_images, name="upload"),
 ]
