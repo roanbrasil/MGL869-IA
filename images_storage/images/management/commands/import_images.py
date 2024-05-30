b91e0e55abb12f046dc67a8cfa2139aab70e8d0c
Augusto Latgé <augusto.latge@erudit.org>
2024-05-30 11:41:47 -0400

    Fix images repository location

--- images_storage/images/management/commands/import_images.py
+++ images_storage/images/management/commands/import_images.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 import shutil
 from django.contrib.auth.models import User
-from django.conf import settings
-from django.core.files import File
 from django.core.files.base import ContentFile
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from images.models import Image, CATEGORIES, PROCESSES
 
 
@@ -21,10 +19,6 @@ class Command(BaseCommand):
         validation_images_src = images_src / "seg_test"
         for i in CATEGORIES.values:
             for image_src in (training_images_src / CATEGORIES(i).label).iterdir():
-                image_dest = (
-                    Path(settings.BASE_DIR, "images_repository") / image_src.name
-                )
-                shutil.copy(image_src, image_dest)
                 image = Image.objects.create(
                     category=CATEGORIES(i).value,
                     user=admin_user,
@@ -32,10 +26,6 @@ class Command(BaseCommand):
                 )
                 image.src.save(image_src.name, ContentFile(image_src.read_bytes()))
             for image_src in (validation_images_src / CATEGORIES(i).label).iterdir():
-                image_dest = (
-                    Path(settings.BASE_DIR, "images_repository") / image_src.name
-                )
-                shutil.copy(image_src, image_dest)
                 image = Image.objects.create(
                     category=CATEGORIES(i).value,
                     user=admin_user,
