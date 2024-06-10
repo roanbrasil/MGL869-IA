2a918fdaaa0560410ba1f954c2ffc855f6ccf6e5
Augusto Latgé <augusto.latge@erudit.org>
2024-06-10 07:17:45 -0400

    Model update

--- model_api/model/classification.py
+++ model_api/model/classification.py
@@ -4,7 +4,7 @@ import numpy as np
 import keras
 from pathlib import Path
 
-model_name = "production_model_50x50_batch_96_epochs_19"
+model_name = "cnn1_model_50x50_batch_96_epochs_19"
 
 classification_module_path = os.path.realpath(__file__)
 
